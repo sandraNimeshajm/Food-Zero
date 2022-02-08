089e94953c9a2b907f5a3b2eddc8f2a5f9b60630
Sandra <nimeshajm.sandra@gmail.com>
2022-02-08 21:43:08 +0100

    blog-two-columns page - section blog-post added

--- src/ts/components/slider.ts
+++ src/ts/components/slider.ts
@@ -1,9 +1,10 @@
 import Swiper from "swiper/bundle";
 
 const slider = () => {
-  const slides = document.querySelector(".js-testimonials-slider");
+  // const slides = document.querySelector(".js-testimonials-slider");
+  // const blogSlides = document.querySelector(".js-blog-post-slider");
 
-  if (!slides) return;
+  // if (!slides || !blogSlides) return;
 
   const swiper = new Swiper(".js-testimonials-slider", {
     slidesPerView: 1,
@@ -15,6 +16,24 @@ const slider = () => {
     navigation: {
       nextEl: ".js-testimonials-next",
       prevEl: ".js-testimonials-prev",
+    },
+  });
+
+  const blogSlider = new Swiper(".js-blog-post-slider", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+
+    navigation: {
+      nextEl: ".blog-post__button-next",
+      prevEl: ".blog-post__button-prev",
+    },
+
+    pagination: {
+      el: ".blog-post__pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
     },
   });
 };
